acce955e74909409f230c9fe76a8c0113da3f6e0
KenichiroArai <sammyleejunior@gmail.com>
2022-05-07 17:02:41 +0900

    (refs #4126) No002_ThreeStageScreenTradingSystemに伴う実装全般
    シミュレーションの整理

--- src/main/java/kmg/im/stock/core/domain/logic/ImStkSimLogic.java
+++ src/main/java/kmg/im/stock/core/domain/logic/ImStkSimLogic.java
@@ -1,7 +1,6 @@
 package kmg.im.stock.core.domain.logic;
 
 import kmg.im.stock.core.domain.model.ImStkStockBrandModel;
-import kmg.im.stock.core.domain.model.ImStkStockPriceTimeSeriesModel;
 import kmg.im.stock.core.infrastructure.exception.ImStkDomainException;
 
 /**
@@ -26,41 +25,5 @@ public interface ImStkSimLogic {
      *                              投資株式ドメイン例外
      */
     ImStkStockBrandModel getImStkStockBrandModel(long stockCode) throws ImStkDomainException;
-
-    /**
-     * 第１のスクリーンに掛ける<br>
-     *
-     * @author KenichiroArai
-     * @sine 1.0.0
-     * @version 1.0.0
-     * @param imStkStockPriceTimeSeriesModel
-     *                                       投資株式株価時系列モデル
-     * @return true：スクリーンに引っかる、false：スクリーンに引っかからない
-     */
-    boolean hangOnFirstScreen(ImStkStockPriceTimeSeriesModel imStkStockPriceTimeSeriesModel);
-
-    /**
-     * 第２のスクリーンに掛ける<br>
-     *
-     * @author KenichiroArai
-     * @sine 1.0.0
-     * @version 1.0.0
-     * @param imStkStockPriceTimeSeriesModel
-     *                                       投資株式株価時系列モデル
-     * @return true：スクリーンに引っかる、false：スクリーンに引っかからない
-     */
-    boolean hangOnSecondScreen(ImStkStockPriceTimeSeriesModel imStkStockPriceTimeSeriesModel);
-
-    /**
-     * 第３のスクリーンに掛ける<br>
-     *
-     * @author KenichiroArai
-     * @sine 1.0.0
-     * @version 1.0.0
-     * @param imStkStockPriceTimeSeriesModel
-     *                                       投資株式株価時系列モデル
-     * @return true：スクリーンに引っかる、false：スクリーンに引っかからない
-     */
-    boolean hangOnThirdScreen(ImStkStockPriceTimeSeriesModel imStkStockPriceTimeSeriesModel);
 
 }
